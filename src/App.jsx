06a5cc724cf6a3428459681b8834e921e9753f5f
Suryakant <surajdwivedi644@gmail.com>
2025-05-13 17:42:43 +0530

    Initilizing the project

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div className='h-screen w-full bg-black text-white '>
+      <h1>Welcome to React</h1>
+    </div>
+  )
+}
+
+export default App
